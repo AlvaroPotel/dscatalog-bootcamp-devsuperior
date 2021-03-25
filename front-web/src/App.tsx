37094e60e5f82463f12c3e32f9ed60500b4dc7bd
alvaropotel <alvaropotel@gmail.com>
2021-03-25 21:19:10 +0000

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/components/Navbar';
 
 
 const App = () => {
-
     return (
-
-        <h1>APP</h1>
+        <Navbar />
     );
 }
 
